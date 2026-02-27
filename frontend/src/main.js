d8af227405fc4f9082229521ddf92fc0179d43e5
Matilda Taipale <matilda.taipale@hotmail.com>
2026-02-27 13:24:39 +0200

    Configure router and register Pinia store

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router";
+import { createPinia } from "pinia";
 import i18n from "./i18n"; // i18n-konfiguraatio kielenvaihdosta varten
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
@@ -10,7 +11,11 @@ import "@/assets/styles.css";
 library.add(faBars, faTimes);
 
 const app = createApp(App);
+
+const pinia = createPinia();  
+
 app.component("FontAwesomeIcon", FontAwesomeIcon); // Register globally
 app.use(i18n);
 app.use(router);
+app.use(pinia); 
 app.mount('#app')
